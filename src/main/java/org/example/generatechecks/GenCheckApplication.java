1137eb71e9debf36d87bfb1568c4f3a5e96d77c8
klopez851 <keidy081lopez@gmail.com>
2025-06-16 07:21:14 -0400

    added a css file, struggling to use it tho :/

--- src/main/java/org/example/generatechecks/GenCheckApplication.java
+++ src/main/java/org/example/generatechecks/GenCheckApplication.java
@@ -2,10 +2,8 @@ package org.example.generatechecks;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
